6ae552ab6667189eefb8d641c7a89609838c46e1
Taha Parsayan <parsayan.t@gmail.com>
2025-02-25 16:25:32 +0100

    Updated for Python3

--- OPETIA.py
+++ OPETIA.py
@@ -1,46 +1,41 @@
-from Tkinter import *
+from tkinter import *
+from tkinter import ttk, filedialog, messagebox
 import os
-import ttk
-import tkFileDialog
-from tkFileDialog import askopenfile
-import tkMessageBox as messagebox
 import myfunctions
 import webbrowser
 import time
 import threading
-
+import subprocess
 
 root = Tk()
 root.geometry("245x445+500+250")
 root.resizable(False, False)
-root. title("OPETIA")
+root.title("OPETIA")
 
-
-#___commands
+# ___Commands
 def btn_mricrogl_command():
-    os.system('./MRIcroGL/MRIcroGL &')
+    subprocess.run(['./MRIcroGL/MRIcroGL'], shell = True, check = True) 
     
 def btn_nifti_organizer_command():
-    os.system('python2 NIFTI_organizer.py &')
+    os.system('python3.8 NIFTI_organizer.py')
     
 def btn_stage1_command():
-    os.system('python2 structural_process.py &')
+    os.system('python3.8 structural_process.py')
 
 def btn_stage2_command():
-    os.system('python2 pet_process.py &')
+    os.system('python3.8 pet_process.py')
 
 def btn_stage3_command():
-    os.system('python2 ROI_analysis.py &')
+    os.system('python3.8 ROI_analysis.py')
 
-#___GUI
-frame1 = Frame(root)
-frame1.config(relief=SUNKEN, bd=2)
+# ___GUI
+frame1 = Frame(root, relief=SUNKEN, bd=2)
 frame1.place(x=5, y=5, width=235, height=435)
 
-btn_mricrogl = Button(frame1, text='MRIcroGL', command = btn_mricrogl_command).place(x=5,y=5, width=220, height = 80)
-btn_nifti_organizer = Button(frame1, text='NIFTI organizer', command = btn_nifti_organizer_command).place(x=5,y=90, width=220, height = 80)
-btn_stage1 = Button(frame1, text='Structural image pre-processing', command = btn_stage1_command).place(x=5,y=175, width=220, height = 80)
-btn_stage2 = Button(frame1, text='PET image pre-processing', command = btn_stage2_command).place(x=5,y=260, width=220, height = 80)
-btn_stage3 = Button(frame1, text='ROI analysis', command = btn_stage3_command).place(x=5,y=345, width=220, height = 80)
+Button(frame1, text='MRIcroGL', command=btn_mricrogl_command).place(x=5, y=5, width=220, height=80)
+Button(frame1, text='NIFTI organizer', command=btn_nifti_organizer_command).place(x=5, y=90, width=220, height=80)
+Button(frame1, text='Structural image pre-processing', command=btn_stage1_command).place(x=5, y=175, width=220, height=80)
+Button(frame1, text='PET image pre-processing', command=btn_stage2_command).place(x=5, y=260, width=220, height=80)
+Button(frame1, text='ROI analysis', command=btn_stage3_command).place(x=5, y=345, width=220, height=80)
 
 root.mainloop()
