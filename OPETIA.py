986919058813fdb3b242d07c4374c29ddf1678c3
Taha Parsayan <taha@Mac.lan>
2025-06-11 00:30:14 +0200

    OPETIA.py updated

--- OPETIA.py
+++ OPETIA.py
@@ -6,16 +6,15 @@ import webbrowser
 import time
 import threading
 import subprocess
+from tkinter import PhotoImage
+from PIL import Image, ImageTk
 
 root = Tk()
-root.geometry("245x445+0+0")
+root.geometry("245x580+0+0")
 root.resizable(False, False)
 root.title("OPETIA")
 
-# ___Commands
-def btn_mricrogl_command():
-    subprocess.run(['./MRIcroGL/MRIcroGL'], shell = True, check = True) 
-    
+# ___Commands    
 def btn_nifti_organizer_command():
     os.system('python3 NIFTI_organizer.py')
     
@@ -30,12 +29,16 @@ def btn_stage3_command():
 
 # ___GUI
 frame1 = Frame(root, relief=SUNKEN, bd=2)
-frame1.place(x=5, y=5, width=235, height=435)
+frame1.place(x=5, y=5, width=235, height=570)
+height = 225; dist = 85
 
-Button(frame1, text='MRIcroGL', command=btn_mricrogl_command).place(x=5, y=5, width=220, height=80)
-Button(frame1, text='NIFTI organizer', command=btn_nifti_organizer_command).place(x=5, y=90, width=220, height=80)
-Button(frame1, text='Structural image pre-processing', command=btn_stage1_command).place(x=5, y=175, width=220, height=80)
-Button(frame1, text='PET image pre-processing', command=btn_stage2_command).place(x=5, y=260, width=220, height=80)
-Button(frame1, text='ROI analysis', command=btn_stage3_command).place(x=5, y=345, width=220, height=80)
+logo = Image.open('logo.png')
+logo = logo.resize((200, 200))
+logo = ImageTk.PhotoImage(logo)
+Label(frame1, image=logo, bg="white", bd=4, relief='raised').place(x=7, y=5, width=215, height=215)
+Button(frame1, text='NIFTI organizer', command=btn_nifti_organizer_command).place(x=5, y=height, width=220, height=80)
+Button(frame1, text='Structural image pre-processing', command=btn_stage1_command).place(x=5, y=height + dist, width=220, height=80)
+Button(frame1, text='PET image pre-processing', command=btn_stage2_command).place(x=5, y=height + dist*2, width=220, height=80)
+Button(frame1, text='ROI analysis', command=btn_stage3_command).place(x=5, y=height + dist*3, width=220, height=80)
 
 root.mainloop()
