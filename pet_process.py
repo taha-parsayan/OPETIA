9b7b98c81e40570020c456128a36dcc099342641
Taha Parsayan <parsayan.t@gmail.com>
2025-06-11 19:03:37 +0200

    fnirt added

--- pet_process.py
+++ pet_process.py
@@ -7,6 +7,7 @@ import time
 import threading
 import subprocess
 import platform
+import fnmatch
 
 root = tk.Tk()
 root.geometry("500x755+0+0")
@@ -15,8 +16,8 @@ root.title("PET image pre-processing")
 
 
 # ___Variables
-var_pet_address_input = tk.StringVar()
-var_pet_address_input.set('Not defined...')
+var_file_address_input = tk.StringVar()
+var_file_address_input.set('Not defined...')
 var_pet_address_output = tk.StringVar()
 var_pet_address_output.set('Not defined...')
 var_BET_threshold = tk.StringVar()
@@ -31,8 +32,6 @@ var_reg_highrez2std_dof = tk.StringVar()
 var_reg_highrez2std_dof.set('Translation only - 3DOF')
 var_reg_matrix_ref_b2c = tk.StringVar()
 var_reg_matrix_ref_b2c.set('Not defined...')
-var_reg_std_ref = tk.StringVar()
-var_reg_std_ref.set('/opt/easybuild/software/FSL/6.0.3-foss-2019b-Python-3.7.4/fsl/data/standard/MNI152lin_T1_2mm_brain.nii.gz')
 var_reg_cost = tk.StringVar()
 var_reg_cost.set('Mutual information')
 var_reg_interp = tk.StringVar()
@@ -45,25 +44,29 @@ var_thrp_check_btn = tk.StringVar()
 var_thrp_check_btn.set(0)
 var_thrp_percentage = tk.StringVar()
 var_thrp_percentage.set('10')
+var_reg_std_ref = tk.StringVar()
+var_reg_std_ref.set("Templates/MNI152_T1_2mm_brain.nii.gz")
+
 
 
 # ___Commands
 def btn_enter_pet_input_command():
-    path = myfunctions.get_address_file()
-    var_pet_address_input.set(path)
-    (dirname, filename) = os.path.split(var_pet_address_input.get())
-    os.system('mkdir ' + dirname + '/OPETIA_output')
-    var_pet_address_output.set(dirname + '/OPETIA_output')
+    path = myfunctions.get_address_folder()
+    var_file_address_input.set(path)
+    var_pet_address_output.set(var_file_address_input.get() + '/OPETIA_output')
     var_reg_matrix_ref_b2c.set(var_pet_address_output.get() + '/structural2std.mat')
     var_reg_pet2highrez_ref.set(var_pet_address_output.get() + '/structural_brain.nii.gz')
 
 def btn_show_image_command():
-    # os.system("fsleyes " + var_pet_address_input.get())
     img1 = var_pet_address_output.get() + '/PET_brain_std.nii.gz'
     img2 = var_pet_address_output.get() + '/structural_brain_std.nii.gz'
     img3 = var_pet_address_output.get() + '/pet_quality_control.png'
     os.system('slicer ' + img1 + ' ' + img2 + ' -A 1500 ' + img3)
-    os.system('eog ' + img3)
+    
+    if platform.system() == 'Linux':
+        os.system(f'eog "{img3}" &')
+    elif platform.system() == 'Darwin':
+        os.system(f'open "{img3}"')
 
 
 def btn_enter_pet_output_command():
@@ -74,9 +77,9 @@ def btn_enter_pet_output_command():
 
 def btn_open_folder_command():
     if is_mac():
-            subprocess.call(["open", var_address_input.get()])
+            subprocess.call(["open", var_file_address_input.get()])
     elif is_linux():
-            subprocess.call(["xdg-open", var_address_input()])
+            subprocess.call(["xdg-open", var_file_address_input()])
 
 def is_mac():
     return platform.system() == 'Darwin'
@@ -84,10 +87,13 @@ def is_mac():
 def is_linux():
     return platform.system() == 'Linux'
 
+def find_PET_volumes(folder):
+    return sorted(fnmatch.filter(os.listdir(folder), 'vol*.nii.gz'))
+
 def btn_process_command():
-    if var_pet_address_input.get() == "Not defined...":
+    if var_file_address_input.get() == "Not defined...":
         messagebox.showinfo("Error...", "Please fill the PET image address")
-    elif var_pet_address_input.get() == "":
+    elif var_file_address_input.get() == "":
         messagebox.showinfo("Error...", "Please fill the PET image address")
     elif var_pet_address_output.get() == "Not defined...":
         messagebox.showinfo("Error...", "Please fill the Output address")
@@ -106,53 +112,100 @@ def btn_process_command():
     elif var_reg_std_ref.get() == "":
         messagebox.showinfo("Error...", "Please fill the registration matrix address")
     else:
-        os.system('echo ')
-        os.system('echo ----------------PET image pre-processing----------------')
-        os.system('echo ')
-        os.system('echo ---Extracting the brain')
-        myfunctions.FSL_Brain_Extraction(var_pet_address_input.get(), var_pet_address_output.get() + '/PET_brain.nii.gz', var_BET_threshold.get(), var_BET_function.get())
-        os.system('echo Done')
+        print('e\n')
+        print('----------------PET image pre-processing----------------')
+        print('--->Extracting the brain')
 
-        os.system('echo ---Registering PET image to native structural space using FLIRT')
-        reg_input = var_pet_address_output.get() + '/PET_brain.nii.gz'
-        reg_output = var_pet_address_output.get() + '/PET_brain_native_structural.nii.gz'
-        reg_ref = var_reg_pet2highrez_ref.get()
+        pet_dir = var_file_address_input.get()
+        pet_vols = find_PET_volumes(pet_dir)
+
+        for vol_name in pet_vols:
+            input_file = os.path.join(pet_dir, vol_name)
+            output_file = os.path.join(var_pet_address_output.get(), vol_name.replace('.nii.gz', '_brain.nii.gz'))
+            myfunctions.FSL_Brain_Extraction(input_file, output_file, var_BET_threshold.get(), var_BET_function.get())
+        print('Done')
+
+        print('--->Registering PET image to native structural space using FLIRT')
+
+        reg_ref = var_reg_pet2highrez_ref.get()  # T1.nii.gz
         reg_model = var_reg_pet2highrez_dof.get()
-        matrix_name = var_pet_address_output.get() + '/pet2structural.mat'
         reg_cost = var_reg_cost.get()
         reg_interp = var_reg_interp.get()
-        myfunctions.register_flirt(reg_input, reg_output, reg_ref, reg_model, matrix_name, reg_cost, reg_interp)
-        os.system('echo Done')
 
-        os.system('echo ---Registering PET image to standard space using transformation matrix')
-        reg_input = var_pet_address_output.get() + '/PET_brain.nii.gz'
-        reg_output = var_pet_address_output.get() + '/PET_brain_std.nii.gz'
-        reg_mat_a2b = var_pet_address_output.get() + '/pet2structural.mat'
-        reg_mat_b2c = var_reg_matrix_ref_b2c.get()
-        reg_ref = var_reg_std_ref.get()
+        # Update PET volume list to skull-stripped versions
+        pet_vols = [vol.replace('.nii.gz', '_brain.nii.gz') for vol in pet_vols]
+        num_vols = len(pet_vols)
+        print(f'Found {num_vols} PET volumes')
 
-        os.system('convert_xfm -omat ' + var_pet_address_output.get() + '/pet2std.mat ' + '-concat ' + reg_mat_b2c + ' ' + reg_mat_a2b)
+        if num_vols == 0:
+            print('No PET volumes found!')
+            return
 
-        reg_mat_a2c = var_pet_address_output.get() + '/pet2std.mat'
-        myfunctions.register_using_matrix(reg_input, reg_output, reg_mat_a2c, reg_ref)
+        if num_vols == 1:
+            # Single volume case: register directly to T1
+            input_file = os.path.join(var_pet_address_output.get(), pet_vols[0])
+            output_file = os.path.join(var_pet_address_output.get(), 'PET.nii.gz')
+            matrix_file = os.path.join(var_pet_address_output.get(), 'pet2structural.mat')
+            myfunctions.register_flirt(input_file, output_file, reg_ref, reg_model, matrix_file, reg_cost, reg_interp)
+            print('Done')
 
-        os.system('echo Done')
+        else:
+            # Multiple volumes
+            registered_vols = []
+
+            # Step 1: Register vol0000 to T1
+            input_file_0 = os.path.join(var_pet_address_output.get(), pet_vols[0])
+            output_file_0 = os.path.join(var_pet_address_output.get(), 'PET_reg_0.nii.gz')
+            matrix_file_0 = os.path.join(var_pet_address_output.get(), 'pet2structural.mat')
+            myfunctions.register_flirt(input_file_0, output_file_0, reg_ref, reg_model, matrix_file_0, reg_cost, reg_interp)
+            registered_vols.append(output_file_0)
+
+            # Step 2: Register remaining volumes to PET_reg_0
+            for i, vol_name in enumerate(pet_vols[1:], start=1):
+                input_file = os.path.join(var_pet_address_output.get(), vol_name)
+                output_file = os.path.join(var_pet_address_output.get(), f'PET_reg_{i}.nii.gz')
+                matrix_file = os.path.join(var_pet_address_output.get(), f'pet2pet_reg_{i}.mat')
+                myfunctions.register_flirt(input_file, output_file, output_file_0, '6', matrix_file, reg_cost, reg_interp)
+                registered_vols.append(output_file)
+
+            # Step 3: Sum all registered PET volumes
+            output_pet = os.path.join(var_pet_address_output.get(), 'PET_brain.nii.gz')
+            cmd = ['fslmaths', registered_vols[0]]
+            for vol in registered_vols[1:]:
+                cmd += ['-add', vol]
+            cmd.append(output_pet)
+            os.system(' '.join(cmd))
+            print('Done')
+
+        print('--->Warping PET image to MNI space using T1-to-MNI warp')
+
+        # Paths
+        reg_input = os.path.join(var_pet_address_output.get(), 'PET_brain.nii.gz')           # PET already in T1 space
+        reg_output = os.path.join(var_pet_address_output.get(), 'PET_brain_std.nii.gz')      # Output in MNI space
+        reg_ref = var_reg_std_ref.get()                                                      # MNI standard reference (e.g., MNI152_T1_2mm_brain.nii.gz)
+        warp_file = os.path.join(var_pet_address_output.get(), 'structural_brain_std_warpcoef.nii.gz')  # FNIRT warp output from T1 to MNI
+
+        # Apply warp
+        os.system(
+            f'applywarp --in={reg_input} --ref={reg_ref} --warp={warp_file} --out={reg_output}'
+        )
+
+        print('Done')
 
         if var_smooth_check_btn.get() == '1':
-            os.system('echo ---Gaussian smoothing the registered PET image')
+            print('--->Gaussian smoothing the registered PET image')
             smooth_sigma = float(var_smooth_fwhm.get()) / 2.3548
             os.system('fslmaths ' + var_pet_address_output.get() + '/PET_brain_std.nii.gz -kernel gauss ' + str(smooth_sigma) + ' -fmean ' + var_pet_address_output.get() + '/PET_brain_std.nii.gz' )
-            os.system('echo Done')
+            print('Done')
 
         if var_thrp_check_btn.get() == '1':
-            os.system('echo ---Thresholding the registered PET image')
+            print('--->Thresholding the registered PET image')
             os.system('fslmaths ' + var_pet_address_output.get() + '/PET_brain_std.nii.gz -thrp ' + var_thrp_percentage.get() + ' ' + var_pet_address_output.get() + '/PET_brain_std.nii.gz' )
-            os.system('echo Done')
+            print('Done')
 
-        os.system('echo ')
-        os.system('echo ----------Finish!')
-        os.system('echo --------------------------------------------')
-        os.system('echo ')
+        print('\n')
+        print('Finish!')
+        print('--------------------------------------------')
 
 
 def btn_reg_structural_ref_command():
@@ -161,7 +214,7 @@ def btn_reg_structural_ref_command():
 
 
 def btn_reg_matrix_ref_command():
-    path = myfunctions.get_address_file_reg_matrix((var_pet_address_input.get()))
+    path = myfunctions.get_address_file_reg_matrix((var_file_address_input.get()))
     var_reg_matrix_ref_b2c.set(path)
 
 
@@ -185,11 +238,11 @@ frame3.place(x=5, y=265, width=490, height=280)
 
 frame4 = tk.LabelFrame(root, text = 'Increase image signal to noise ratio')
 frame4.config(relief=tk.SUNKEN, bd=2)
-frame4.place(x=5, y=560, width=490, height=75)
+frame4.place(x=5, y=560, width=490, height=80)
 
-label1 = tk.Label(frame1, text = "Input PET image:").place(x=5,y=5)
+label1 = tk.Label(frame1, text = "Folder containing the PET volumes:").place(x=5,y=5)
 btn_enter_pet_input = tk.Button(frame1, text = "Browse", command = btn_enter_pet_input_command).place(x=5, y=30)
-entr_address_structural_input = tk.Entry(frame1, textvariable = var_pet_address_input).place(x=100, y=35, width=365)
+entr_address_structural_input = tk.Entry(frame1, textvariable = var_file_address_input).place(x=100, y=35, width=365)
 
 
 label2 = tk.Label(frame1, text = "Output folder:").place(x=5, y=70)
@@ -203,32 +256,32 @@ label4 = tk.Label(frame2, text="Brain extraction function:").place(x=5, y=30)
 combo_BET_function = ttk.Combobox(frame2, textvariable=var_BET_function, values=('Standard brain extraction using bet2', 'Robust brain centre estimation',
 'Eye & optic nerve cleanup', 'Bias field & neck cleanup')).place(x=200, y=30, width=260)
 
-label5 = tk.Label(frame3, text = "PET to structural space registration:").place(x=5, y=5)
+label5 = tk.Label(frame3, text = "Brain extracted T1 in native space (structural_brain.nii.gz):").place(x=5, y=5)
 btn_reg_structural_ref = tk.Button(frame3, text = "Browse", command = btn_reg_structural_ref_command).place(x=5, y=30)
 entr_reg_structural_ref = tk.Entry(frame3, textvariable = var_reg_pet2highrez_ref).place(x=100, y=30, width=365)
 
-label6 = tk.Label(frame3, text = "PET to structural registration matrix:").place(x=5, y=60)
-btn_reg_matrix_ref = tk.Button(frame3, text = "Browse", command = btn_reg_matrix_ref_command).place(x=5, y=85)
-entr_reg_matrix_ref = tk.Entry(frame3, textvariable = var_reg_matrix_ref_b2c).place(x=100, y=85, width=365)
+# label6 = tk.Label(frame3, text = "PET to structural registration matrix:").place(x=5, y=60)
+# btn_reg_matrix_ref = tk.Button(frame3, text = "Browse", command = btn_reg_matrix_ref_command).place(x=5, y=85)
+# entr_reg_matrix_ref = tk.Entry(frame3, textvariable = var_reg_matrix_ref_b2c).place(x=100, y=85, width=365)
 
-label7 = tk.Label(frame3, text = "Standard image for registration:").place(x=5, y=115)
+label7 = tk.Label(frame3, text = "Standard template (MNI152_T1_2mm_brain.nii.gz):").place(x=5, y=115)
 btn_reg_std_ref = tk.Button(frame3, text = "Browse", command = btn_reg_std_ref_command).place(x=5, y=140)
 entr_reg_std_ref = tk.Entry(frame3, textvariable = var_reg_std_ref).place(x=100, y=140, width=365)
 
-label8 = tk.Label(frame3, text = "Registration model (DOF):").place(x=5, y=170)
+label8 = tk.Label(frame3, text = "PET to T1 registration model (DOF):").place(x=5, y=170)
 combo_reg_pet2highrez_dof = ttk.Combobox(frame3, textvariable=var_reg_pet2highrez_dof, values=('Rigid body - 6DOF', 'Affine - 12DOF', 'Nonlinear - 9DOF')).place(x=267, y=170, width=200)
 
-label9 = tk.Label(frame3, text = "Cost function:").place(x=5, y=200)
+label9 = tk.Label(frame3, text = "PET to T1 Cost function:").place(x=5, y=200)
 combo_reg_cost = ttk.Combobox(frame3, textvariable=var_reg_cost, values=('Mutual information', 'Least squares', 'Normalized mutual information')).place(x=267, y=200, width=200)
 
-label10 = tk.Label(frame3, text = "Interpolation method:").place(x=5, y=230)
+label10 = tk.Label(frame3, text = "PET to T1 interpolation method:").place(x=5, y=230)
 combo_reg_interp = ttk.Combobox(frame3, textvariable=var_reg_interp, values=('Tri-linear', 'Nearest neighbor', 'Spline')).place(x=267, y=230, width=200)
 
-label11 = tk.Label(frame4, text="Smooth PET image").place(x=30,y=5)
+label11 = tk.Label(frame4, text="Smooth the PET image").place(x=30,y=5)
 check_smooth = tk.Checkbutton(frame4, variable=var_smooth_check_btn, onvalue='1', offvalue='0').place(x=5, y=5)
 entr_smooth_fwhm = tk.Entry(frame4, textvariable=var_smooth_fwhm).place(x=225, y=5, width=60)
 
-label12 = tk.Label(frame4, text="Threshold PET image").place(x=30,y=30)
+label12 = tk.Label(frame4, text="Threshold the PET image").place(x=30,y=30)
 check_thrp = tk.Checkbutton(frame4, variable=var_thrp_check_btn, onvalue='1', offvalue='0').place(x=5, y=30)
 entr_thrp_percentage = tk.Entry(frame4, textvariable=var_thrp_percentage).place(x=225, y=30, width=60)
 
