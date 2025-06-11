40e2fc1c43cad9c0bf35c79a562ee118110715a4
Taha Parsayan <taha@Mac.lan>
2025-06-11 09:22:09 +0200

    Nifti Organizer updated to work for both macos and linux

--- NIFTI_organizer.py
+++ NIFTI_organizer.py
@@ -9,6 +9,7 @@ import subprocess
 import shutil
 import fnmatch
 import sys
+import platform
 
 root = Tk()
 root.geometry("500x195+0+0")
@@ -28,83 +29,99 @@ def btn_enter_address_command():
 
 def btn_open_folder_command():
     webbrowser.open(var_address_input.get())
-    
+
+def is_mac():
+    return platform.system() == 'Darwin'
+
+def is_linux():
+    return platform.system() == 'Linux'
+
 def btn_process_command():
-    print('----------------Nifti organizer----------------')
-    
-    print('  ')
+    print('----------------Nifti organizer----------------\n')
+
     print('---Moving Nifti files to subject folders')
     main_directory = var_address_input.get()
-    
+
+    # Move files to subject folders
     for subject_folder in os.listdir(main_directory):
-        subject_path = os.path.join(main_directory, subject_folder)
-        if not os.path.isdir(subject_path):
+        if subject_folder.startswith('.') or not os.path.isdir(os.path.join(main_directory, subject_folder)):
             continue
+        subject_path = os.path.join(main_directory, subject_folder)
 
         for file in os.listdir(main_directory):
-            file_path = os.path.join(main_directory, file)
-            if os.path.isdir(file_path) or not file.endswith('.nii'):
+            if file.startswith('.') or not file.endswith('.nii.gz'):
                 continue
-
+            file_path = os.path.join(main_directory, file)
+            if os.path.isdir(file_path):
+                continue
             if subject_folder in file:
                 new_file_path = os.path.join(subject_path, os.path.basename(file))
                 shutil.move(file_path, new_file_path)
-                          
+
     print('---Renaming the files to T1 and PET & unzipping PET')
     t1_keywords = ['t1', 'mri', 'mprage', 'mp-rage', 'mp_rage']
     pet_keywords = ['pet', 'fdg', 'f-18', 'static']
-    
+
     def contains_keyword(filename, keywords):
         return any(keyword in filename.lower() for keyword in keywords)
-       
+
     for item in os.listdir(main_directory):
+        if item.startswith('.'):
+            continue
         item_path = os.path.join(main_directory, item)
         if os.path.isdir(item_path):
-            nii_count = sum(1 for file in os.listdir(item_path) if file.endswith('.nii'))
+            nii_count = sum(1 for file in os.listdir(item_path) if file.endswith('.nii.gz'))
             if nii_count == 2:
                 for file in os.listdir(item_path):
+                    if not file.endswith('.nii.gz'):
+                        continue
                     file_path = os.path.join(item_path, file)
-                    if file.endswith('.nii'):
-                        if contains_keyword(file, t1_keywords):
-                            new_name = 'T1.nii'
-                        elif contains_keyword(file, pet_keywords):
-                            new_name = 'PET.nii' 
-                        else:
-                            continue
+                    if contains_keyword(file, t1_keywords):
+                        new_name = 'T1.nii.gz'
+                    elif contains_keyword(file, pet_keywords):
+                        new_name = 'PET.nii.gz'
+                    else:
+                        continue
 
-                        new_path = os.path.join(item_path, new_name)
-                        if file_path != new_path:
-                            os.rename(file_path, new_path)
-                        
-                        path_PET = os.path.join(item_path, 'PET.nii')
-                        if os.path.exists(path_PET):
-                            subprocess.run(['fslsplit', path_PET, os.path.join(item_path, 'vol')])
-                            subprocess.run(['gunzip'] + list(fnmatch.filter(os.listdir(item_path), 'vol*.nii.gz')))
-                            path_vol = os.path.join(item_path, 'vol0000.nii.gz')
-                            if os.path.exists(path_vol):
-                                subprocess.run(['rm'] + list(fnmatch.filter(os.listdir(item_path), 'vol*.nii.gz')))
-                    
-            elif nii_count != 2:
+                    new_path = os.path.join(item_path, new_name)
+                    if file_path != new_path:
+                        os.rename(file_path, new_path)
+
+                # Process PET
+                path_PET = os.path.join(item_path, 'PET.nii.gz')
+                if os.path.exists(path_PET):
+                    subprocess.run(['fslsplit', path_PET, os.path.join(item_path, 'vol')])
+                    # gunzip each vol*.nii.gz file
+                    for f in fnmatch.filter(os.listdir(item_path), 'vol*.nii.gz'):
+                        subprocess.run(['gunzip', os.path.join(item_path, f)])
+                    # Remove .nii.gz files (if still exist)
+                    for f in fnmatch.filter(os.listdir(item_path), 'vol*.nii.gz'):
+                        os.remove(os.path.join(item_path, f))
+
+            else:
                 black_list.append(item_path)
-    
-    print('Done')
 
+    print('Done\n')
+
+    # Verification
     for folder_name in os.listdir(main_directory):
-        address1 = os.path.join(main_directory, folder_name, 'T1.nii')
-        address2 = os.path.join(main_directory, folder_name, 'PET.nii')
+        if folder_name.startswith('.') or not os.path.isdir(os.path.join(main_directory, folder_name)):
+            continue
+        folder_path = os.path.join(main_directory, folder_name)
+        address1 = os.path.join(folder_path, 'T1.nii.gz')
+        address2 = os.path.join(folder_path, 'PET.nii.gz')
+
         if not os.path.exists(address1):
-            print('  ')
-            print('------ Could not create: '+ address1)
+            print('------ Could not create:', address1)
             if var_check_open_files.get() == '1':
-                webbrowser.open(os.path.join(main_directory, folder_name))
-        
+                webbrowser.open(folder_path)
+
         if not os.path.exists(address2):
-            print('  ')
-            print('------ Could not create: '+ address2)
+            print('------ Could not create:', address2)
             if var_check_open_files.get() == '1':
-                webbrowser.open(os.path.join(main_directory, folder_name))
-    
-    print('Done')
+                webbrowser.open(folder_path)
+
+    print("Done")
 
 #___GUI
 frame1 = LabelFrame(root, text='Input parameters', relief=SUNKEN, bd=2)
