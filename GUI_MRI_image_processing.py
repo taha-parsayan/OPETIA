2a4cfa0c4f8cb9a1a5270b41cdf96ed6ae370572
Taha Parsayan <parsayan.t@gmail.com>
2025-08-24 08:48:11 +0200

    GUI updated

--- GUI_MRI_image_processing.py
+++ GUI_MRI_image_processing.py
@@ -35,10 +35,12 @@ app.resizable(False, False)
 # Global Variables
 #------------------------------
 
-var_T1_path = tk.StringVar()
+var_T1_path = ctk.StringVar()
 var_T1_path.set("Enter path to T1.nii.gz")
-var_output_path = tk.StringVar()
+var_output_path = ctk.StringVar()
 var_output_path.set("Enter path to output folder")
+var_mri_modality = ctk.StringVar()
+var_mri_modality.set("T1 weighted")
 
 # -------------------------------
 # Function to run when button is clicked
@@ -72,10 +74,18 @@ def set_output_address():
     else:
         messagebox.showinfo("Error...", "Invalid folder path!")
 
+def set_modality(choice):
+    if choice == "T1 weigted":
+        mri_modality = "t1"
+    elif choice == "T2 weighted":
+        mri_modality = "t2"
+    elif choice == "FLAIR":
+        mri_modality = "flair"
 
 #------------------------------
 # GUI Elements
 #------------------------------
+
 frame1 = ctk.CTkFrame(master=app, width=390, height=150, border_color="#ffffff", border_width=1)
 frame1.place(x=5, y=5)
 
@@ -85,20 +95,34 @@ lable1.place(x=5, y=5)
 btn1 = ctk.CTkButton(master=frame1, text="Browse", width=100, height=25, command=get_MRI_address)
 btn1.place(x=5, y=40)
 
-entry1 = ctk.CTkEntry(master=frame1, textvariable = var_T1_path, width = 250, height=25)
+entry1 = ctk.CTkEntry(master=frame1, textvariable = var_T1_path, width = 265, height=25)
 entry1.place(x=120, y=40)
 
-lable2 = ctk.CTkLabel(master=frame1, text="Set output image address:", font=("Times New Roman", 15))
+lable2 = ctk.CTkLabel(master=frame1, text="Set output folder address:", font=("Times New Roman", 15))
 lable2.place(x=5, y=75)
 
 btn2 = ctk.CTkButton(master=frame1, text="Browse", width=100, height=25, command=set_output_address)
 btn2.place(x=5, y=110)
 
-entry2 = ctk.CTkEntry(master=frame1, textvariable=var_output_path, width = 250, height=25)
+entry2 = ctk.CTkEntry(master=frame1, textvariable=var_output_path, width = 265, height=25)
 entry2.place(x=120, y=110)
+
+
+#-------------------------------------
+
+frame2 = ctk.CTkFrame(master=app, width=390, height=45, border_color="#ffffff", border_width=1)
+frame2.place(x=5, y=160)
+
+lable3 = ctk.CTkLabel(master=frame2, text="MRI image modality:", font=("Times New Roman", 15))
+lable3.place(x=5, y=5)
+
+combobox1 = ctk.CTkComboBox(master=frame2, width=235, height=25,
+                                     values=["T1 weigted", "T2 weighted", "FLAIR"],
+                                     command=set_modality,
+                                     variable=var_mri_modality)
+combobox1.place(x=150, y=10)
 
 # Run the app
 app.mainloop()
-
 
 
--- Image_Processing_Functions.py
+++ Image_Processing_Functions.py
@@ -10,9 +10,27 @@ import ants
 import antspynet
 
 #------------------------------
+# Skull Stripping using ANTsPyNet
+#------------------------------
+def skull_strip(img_address, output_address, modality="t1"):
+
+    # Load image
+    img = ants.image_read(img_address)
+
+    # Brain extraction
+    # Modalities: "t1", "t2", "flair"
+    brain_mask = antspynet.brain_extraction(img, modality)
+    img_brain = img * brain_mask
+
+    # Save the skull-stripped image
+    ants.image_write(img_brain, output_address)
+
+    return img_brain
+
+#------------------------------
 # Image Registration to MNI Space
 #------------------------------
-def register_to_MNI(img_address, output_address):
+def register_to_MNI(img_address, output_address, registration_type="SyN"):
 
     # Working directory
     current_dir = os.getcwd()
@@ -22,10 +40,28 @@ def register_to_MNI(img_address, output_address):
     MNI = ants.image_read(current_dir + "/Templates/MNI152_T1_2mm.nii.gz")
 
     # Perform registration (T1 → MNI)
+    """
+    registration_type:
+
+    Linear (Rigid / Affine):
+        "Translation" → Translation only (shifts)
+        "Rigid" → Rigid-body (rotation + translation)
+        "Similarity" → Rigid + uniform scaling
+        "Affine" → Affine (rigid + scaling + shearing)
+    
+    Nonlinear (Deformable)
+        "ElasticSyN" → Elastic deformation using SyN
+        "SyN" → Symmetric normalization (nonlinear warp, very common for T1→MNI)
+        "SyNOnly" → Just nonlinear deformation (no affine initialization)
+        "SyNCC" → SyN using cross-correlation metric (good for intra-modality MRI)
+        "SyNRA" → SyN with rigid + affine initialization
+        "SyNAggro" → More aggressive version of SyN (stronger warps)
+        "SyNabp" → SyN optimized for b0-dMRI → T1 registration
+    """
     reg = ants.registration(
         fixed=MNI,        # target (MNI space)
         moving=img,        # source (subject T1)
-        type_of_transform="SyN"   # nonlinear transform (symmetric normalization)
+        type_of_transform=registration_type
     )
 
     # Access the warped moving image (subject in MNI space)
@@ -34,23 +70,6 @@ def register_to_MNI(img_address, output_address):
     ants.image_write(img_reg, output_address)
 
     return img_reg
-
-#------------------------------
-# Skull Stripping using ANTsPyNet
-#------------------------------
-def skull_strip(img_address, output_address):
-
-    # Load image
-    img = ants.image_read(img_address)
-
-    # Brain extraction
-    brain_mask = antspynet.brain_extraction(img, modality="t1")
-    img_brain = img * brain_mask
-
-    # Save the skull-stripped image
-    ants.image_write(img_brain, output_address)
-
-    return img_brain
 
 #------------------------------
 # Tissue Segmentation using Atropos
