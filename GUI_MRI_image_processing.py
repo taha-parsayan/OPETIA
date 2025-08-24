5509d1b1525488235ae3c26736171c6b64b3b975
Taha Parsayan <parsayan.t@gmail.com>
2025-08-24 10:28:13 +0200

    BET and Reg added

--- GUI_MRI_image_processing.py
+++ GUI_MRI_image_processing.py
@@ -35,12 +35,22 @@ app.resizable(False, False)
 # Global Variables
 #------------------------------
 
+""" GUI Elements Variables """
+
 var_T1_path = ctk.StringVar()
 var_T1_path.set("Enter path to T1.nii.gz")
 var_output_path = ctk.StringVar()
 var_output_path.set("Enter path to output folder")
 var_mri_modality = ctk.StringVar()
 var_mri_modality.set("T1 weighted")
+var_reg_class = ctk.StringVar()
+var_reg_class.set("Nonlinear")
+var_reg_type = ctk.StringVar()
+var_reg_type.set("Elastic deformation using SyN")
+
+""" Other Global Variables """
+mri_modality = "t1"
+registration_type = "SyN"
 
 # -------------------------------
 # Function to run when button is clicked
@@ -52,12 +62,10 @@ def get_address_file():
         return os.path.abspath(file.name)
     return None
 
-
 def get_address_folder():
     return filedialog.askdirectory()
 
-
-def get_MRI_address():
+def set_MRI_address():
     address = get_address_file()
     if address:
         var_T1_path.set(address)
@@ -82,6 +90,28 @@ def set_modality(choice):
     elif choice == "FLAIR":
         mri_modality = "flair"
 
+def set_reg_combo_box_type(choice):
+    if choice == "Linear":
+        keys = list(linear_options_dict.keys())
+        combobox3.configure(values=keys)
+        var_reg_type.set(keys[0])
+        set_reg_type(var_reg_type.get())
+    else:  # Nonlinear
+        keys = list(nonlinear_options_dict.keys())
+        combobox3.configure(values=keys)
+        var_reg_type.set(keys[0])
+        set_reg_type(var_reg_type.get())
+
+def set_reg_type(registration_type):
+    registration_class = var_reg_class.get() # Linear or Nonlinear
+    if registration_class == "Linear":
+        registration_type = linear_options_dict[registration_type]
+    else:  # Nonlinear
+        registration_type = nonlinear_options_dict[registration_type]
+
+    print(f"Selected registration type: {registration_type}")
+
+
 #------------------------------
 # GUI Elements
 #------------------------------
@@ -92,7 +122,7 @@ frame1.place(x=5, y=5)
 lable1 = ctk.CTkLabel(master=frame1, text="Set T1.nii.gz image address:", font=("Times New Roman", 15))
 lable1.place(x=5, y=5)
 
-btn1 = ctk.CTkButton(master=frame1, text="Browse", width=100, height=25, command=get_MRI_address)
+btn1 = ctk.CTkButton(master=frame1, text="Browse", width=100, height=25, command=set_MRI_address)
 btn1.place(x=5, y=40)
 
 entry1 = ctk.CTkEntry(master=frame1, textvariable = var_T1_path, width = 265, height=25)
@@ -121,6 +151,47 @@ combobox1 = ctk.CTkComboBox(master=frame2, width=235, height=25,
                                      command=set_modality,
                                      variable=var_mri_modality)
 combobox1.place(x=150, y=10)
+
+#-------------------------------------
+
+frame3 = ctk.CTkFrame(master=app, width=390, height=135, border_color="#ffffff", border_width=1)
+frame3.place(x=5, y=210)
+
+# Linear options mapping
+linear_options_dict = {
+    "Translation (shifts)": "Translation",
+    "Rigid-body (rotation + translation)": "Rigid",
+    "Rigid + uniform scaling": "Similarity",
+    "Affine (rigid + scaling + shearing)": "Affine"
+}
+
+# Nonlinear options mapping
+nonlinear_options_dict = {
+    "Elastic deformation using SyN": "ElasticSyN",
+    "Symmetric normalization (nonlinear warp)": "SyN",
+    "Nonlinear deformation (no affine initialization)": "SyNOnly",
+    "SyN using cross-correlation metric": "SyNCC",
+    "SyN with rigid + affine initialization": "SyNRA",
+    "More aggressive SyN (stronger warps)": "SyNAggro",
+    "SyN optimized for b0-dMRI → T1 registration": "SyNabp"
+}
+
+label4 = ctk.CTkLabel(master=frame3, text="Select Registration Class:", font=("Times New Roman", 15))
+label4.place(x=5, y=5)
+
+combobox2 = ctk.CTkComboBox(master=frame3, width=380, values=["Nonlinear", "Linear"],
+                                 variable=var_reg_class,
+                                 command=set_reg_combo_box_type)
+combobox2.place(x=5, y=35)
+
+label5 = ctk.CTkLabel(master=frame3, text="Select Registration Type:", font=("Times New Roman", 15))
+label5.place(x=5, y=65)
+
+combobox3 = ctk.CTkComboBox(master=frame3, width=380, values=list(nonlinear_options_dict.keys()),
+                                variable=var_reg_type,
+                                command=set_reg_type)
+combobox3.place(x=5, y=95)
+
 
 # Run the app
 app.mainloop()
